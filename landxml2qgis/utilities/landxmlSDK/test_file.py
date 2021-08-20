ab24ecdfabfbbef32db9e225c543bce5050ccff0
james.k.leversha <james.k.leversha@delwp.vic.gov.au>
2021-08-20 18:43:17 +1000

    fixed misclose issue

--- landxml2qgis/utilities/landxmlSDK/test_file.py
+++ landxml2qgis/utilities/landxmlSDK/test_file.py
@@ -8,14 +8,6 @@ import boto3
 import pickle as pkl
 import requests
 
-
-# import requests
-# url = 'https://dcm-file-sharing.s3.amazonaws.com/YARRA%20RANGES/LP089369.xml'
-# headers = {'Host': 'dcm-file-sharing.s3.ap-southeast-2.amazonaws.com'}
-# r = requests.get(url, headers=headers)
-# print(r.status_code)
-
-import boto3
-s3 = boto3.resource('s3')
-my_bucket = s3.Bucket('dcm-file-sharing')
-my_bucket.download_file(f'{lga}/{aws_name}.xml', f'{self.outpath}/{aws_name}.xml')
+infile = '/Users/jamesleversha/Downloads/test_download/DAREBIN/BATCH_1/TP423087N/kujQZkmH5.1ppM1GGOHwKFBoq5KUyau7/TP423087N.xml'
+data = landxml.parse(infile, silence=True, print_warnings=False)
+geometries = Geometries(data, mis_tol=.1)
